41df8ab088f35fc716f45018c7de34de65f23f3e
Abhishek  Singh <abhik.msrit@gmail.com>
2022-10-06 19:08:06 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'assign1';
   
+  //Log the number of 'Display details' button clicks. missing 
+  //Every fifth item will have a blue background. missing
+  //not clear input when we click on add button
+  //Try to not use Any Type
+  //clean your code and remove comments
+  //
+  
 }
